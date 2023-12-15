6af453125b4e2b5009ddb401a520b81eecb73810
AndresFelipeGA <andresfgonzalezaristizabal@gmail.com>
2023-12-14 19:17:25 -0500

    About

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
+import About from './components/About';
+import Skills from './components/Skills';
 
 const App = () => {
   return <div>
     <Header />
     <Hero />
     <Brands />
+    <About />
+    <Skills />
     <div style={{ height: '2000px' }}></div>
   </div>;
 };
